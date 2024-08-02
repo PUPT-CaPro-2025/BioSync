2a60f0e794f07fae037cc811537f5099a548f611
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-02 20:28:18 +0800

    remove some imports

--- BioSync.UI/src/app/homepage/homepage.component.ts
+++ BioSync.UI/src/app/homepage/homepage.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from '../sidenav/sidenav.component';
-import { ScheduleComponent } from '../schedule/schedule.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, SidenavComponent, ScheduleComponent, MatSidenavModule],
+  imports: [RouterOutlet, SidenavComponent, MatSidenavModule ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
