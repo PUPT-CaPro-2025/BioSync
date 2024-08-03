47ba558e82a068ac94c11239f69772c683aeba74
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-03 21:33:24 +0800

    Add structure for add subject component

--- BioSync.UI/src/app/add-subject/add-subject.component.ts
+++ BioSync.UI/src/app/add-subject/add-subject.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { SubjectComponent } from '../subject/subject.component';
 
 @Component({
   selector: 'app-add-subject',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatFormFieldModule, MatInputModule, FormsModule, SubjectComponent],
   templateUrl: './add-subject.component.html',
   styleUrl: './add-subject.component.css'
 })
 export class AddSubjectComponent {
+  @Output() backToSubject = new EventEmitter<void>();
 
+  cancelOrAddSubject(): void {
+    this.backToSubject.emit();
+  }
 }
