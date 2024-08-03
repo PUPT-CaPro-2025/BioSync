7943e18849767afcbf4dfcdb7719078494655f8b
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-03 19:50:12 +0800

    Add custom properties

--- BioSync.UI/tailwind.config.js
+++ BioSync.UI/tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkRed: '#9F0303',
+        brightRed: '#D31119',
+        darkOrange: '#E4581D',
+        f2White: 'F2E8E9',
+        f4White: '#F4F4F4',
+        cleanWhite: '#FFFFFF',
+      },
+      fontSize: {
+        'custom-32': '32px',
+        'custom-24': '24px',
+        'custom-20': '20px',
+        'custom-16': '16px',
+        'custom-14': '14px',
+        'custom-12': '12px',
+      },
+    },
   },
   plugins: [],
 }
