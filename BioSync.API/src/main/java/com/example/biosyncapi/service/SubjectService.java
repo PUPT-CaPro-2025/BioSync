313e058389bdf341f06366c972fe9452dc0f6af6
Jhean Galope <drakekhendrick@gmail.com>
2024-08-03 14:52:25 +0800

    setup subject api

---
+++ BioSync.API/src/main/java/com/example/biosyncapi/service/SubjectService.java
@@ -0,0 +1,13 @@
+package com.example.biosyncapi.service;
+
+import com.example.biosyncapi.model.Subject;
+import java.util.List;
+import java.util.Optional;
+
+public interface SubjectService {
+    List<Subject> getAllSubjects();
+    Optional<Subject> getSubjectById(Long id);
+    Subject createSubject(Subject subject);
+    Subject updateSubject(Long id, Subject subject);
+    void deleteSubject(Long id);
+}
---
+++ BioSync.API/src/main/java/com/example/biosyncapi/service/impl/SubjectServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.biosyncapi.service.impl;
+
+import com.example.biosyncapi.model.Subject;
+import com.example.biosyncapi.repository.SubjectRepository;
+import com.example.biosyncapi.service.SubjectService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SubjectServiceImpl implements SubjectService {
+
+    private final SubjectRepository subjectRepository;
+
+    public SubjectServiceImpl(SubjectRepository subjectRepository) {
+        this.subjectRepository = subjectRepository;
+    }
+
+    @Override
+    public List<Subject> getAllSubjects() {
+        return subjectRepository.findAll();
+    }
+
+    @Override
+    public Optional<Subject> getSubjectById(Long id) {
+        return subjectRepository.findById(id);
+    }
+
+    @Override
+    public Subject createSubject(Subject subject) {
+        return subjectRepository.save(subject);
+    }
+
+    @Override
+    public Subject updateSubject(Long id, Subject subject) {
+        if(!subjectRepository.existsById(id))
+            throw new RuntimeException("Subject not found");
+
+        return subjectRepository.save(subject);
+    }
+
+    @Override
+    public void deleteSubject(Long id) {
+        subjectRepository.deleteById(id);
+    }
+}
