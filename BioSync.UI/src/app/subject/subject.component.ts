ee5ea462304427aa68a16baab16d125d8897d8fc
Jhean Galope <drakekhendrick@gmail.com>
2024-08-04 11:49:33 +0800

    update subject model

--- BioSync.UI/src/app/subject/subject.component.ts
+++ BioSync.UI/src/app/subject/subject.component.ts
@@ -15,7 +15,7 @@ import { AddSubjectComponent } from '../add-subject/add-subject.component';
 })
 export class SubjectComponent {
   entries: string[] = [
-    '10', '20', '30', '40', '50'  
+    '10', '20', '30', '40', '50'
   ];
 
   sorting: string[] = [
@@ -26,31 +26,7 @@ export class SubjectComponent {
   subjects: Subject[] = [
     { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
     { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
-    { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
-    { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
-    { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
-    { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
-    { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
-    { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
-    { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
-    { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
-    { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
-    { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
-    { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
-    { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
-    { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
-    { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
-    { subject_code: 'COMP 1103', subject_name: 'Fundamentals of Computing' },
-    { subject_code: 'COMP 2013', subject_name: 'Computer Programming I' },
-    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' },
+    { subject_code: 'COMP 2103', subject_name: 'Information Technology Fundamentals' }
   ];
 
   @Input() totalItems: number = 500;
--- BioSync.UI/src/model/subject-model.ts
+++ BioSync.UI/src/model/subject-model.ts
@@ -1,4 +1,5 @@
 export interface Subject {
     subject_code: string;
     subject_name: string;
+    subject_description?: string;
   }
