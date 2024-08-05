ecd64cac1c437868b21b4aaaeaba60060615979a
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-05 13:06:27 +0800

    Add view schedule component

---
+++ BioSync.UI/src/app/view-schedule/view-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-schedule.component.html',
+  styleUrl: './view-schedule.component.css'
+})
+export class ViewScheduleComponent {
+
+}
