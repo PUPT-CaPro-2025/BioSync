6281dd80e801f0545e4fbbd28e07e307a63140f8
Jhean Galope <drakekhendrick@gmail.com>
2024-08-05 11:31:15 +0800

    setup formbuilder on add sched form

--- BioSync.UI/src/app/add-schedule/add-schedule.component.ts
+++ BioSync.UI/src/app/add-schedule/add-schedule.component.ts
@@ -1,21 +1,18 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {Component, Output, EventEmitter, OnInit} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatSelectModule} from '@angular/material/select';
+import {MatInput} from "@angular/material/input";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-add-schedule',
   standalone: true,
-  imports: [MatToolbarModule, MatSelectModule],
+  imports: [MatToolbarModule, MatSelectModule, MatInput, ReactiveFormsModule],
   templateUrl: './add-schedule.component.html',
   styleUrl: './add-schedule.component.css'
 })
-export class AddScheduleComponent {
+export class AddScheduleComponent implements OnInit{
   @Output() backToSchedule = new EventEmitter<void>();
-
-  cancelOrAddSchedule(): void {
-    this.backToSchedule.emit();
-  }
-
   sections: string[] = [
     'BSIT 4-1',
     'BSIT 3-1',
@@ -39,11 +36,54 @@ export class AddScheduleComponent {
 
   semesters: string[] = [
     '1st Semester',
-    '2nd Semster',
+    '2nd Semester',
     'Summer'
   ];
 
   remarks: string[] = [
     'Laboratory'
   ];
+
+  scheduleForm!: FormGroup;
+  today!: string;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForm();
+    this.getCurrentDate();
+  }
+
+  initForm(): void{
+    this.scheduleForm = this.formBuilder.group({
+        section: ['', [Validators.required]],
+        startTime: ['', Validators.required],
+        endTime: ['', [Validators.required]],
+        scheduleDate: ['', [Validators.required]],
+        labRoom: ['', [Validators.required]],
+        professor: ['', [Validators.required]],
+        semester: ['', [Validators.required]],
+        startYear: [new Date().getFullYear(), [Validators.required]],
+        endYear: [new Date().getFullYear() + 1, [Validators.required]],
+        remarks: ['', [Validators.required]]
+      }
+    )
+  }
+
+  getCurrentDate () {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    this.today = `${year}-${month}-${day}`;
+  }
+
+  cancelAddSchedule(): void {
+    this.backToSchedule.emit();
+  }
+
+  submit() {
+    console.log(this.scheduleForm.value);
+  }
+
 }
