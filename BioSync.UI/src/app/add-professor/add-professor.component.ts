b892784c2105f33b0643075be17233d1197e7cf5
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-06 20:51:43 +0800

    Add "Add professor" component

---
+++ BioSync.UI/src/app/add-professor/add-professor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-professor',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-professor.component.html',
+  styleUrl: './add-professor.component.css'
+})
+export class AddProfessorComponent {
+
+}
