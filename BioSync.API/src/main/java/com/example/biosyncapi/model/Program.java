4f468d7d4ca5df8cb96120f79fd78ef901cb56b6
Jhean Galope <drakekhendrick@gmail.com>
2024-08-06 13:02:21 +0800

    setup program api

---
+++ BioSync.API/src/main/java/com/example/biosyncapi/model/Program.java
@@ -0,0 +1,54 @@
+package com.example.biosyncapi.model;
+
+public class Program {
+
+    private Long id;
+
+    private String programName;
+
+    private String programAbbreviation;
+
+    private String programDescription;
+
+    public Program() {
+    }
+
+    public Program(Long id, String programName, String programAbbreviation, String programDescription) {
+        this.id = id;
+        this.programName = programName;
+        this.programAbbreviation = programAbbreviation;
+        this.programDescription = programDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProgramName() {
+        return programName;
+    }
+
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+
+    public String getProgramAbbreviation() {
+        return programAbbreviation;
+    }
+
+    public void setProgramAbbreviation(String programAbbreviation) {
+        this.programAbbreviation = programAbbreviation;
+    }
+
+    public String getProgramDescription() {
+        return programDescription;
+    }
+
+    public void setProgramDescription(String programDescription) {
+        this.programDescription = programDescription;
+    }
+}
