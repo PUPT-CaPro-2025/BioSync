38a1847d629042e1b09cc90c6e4173db57e9c25d
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-13 19:11:24 +0800

    Add 'edit visitor' component structure

--- BioSync.UI/src/app/edit-visitor/edit-visitor.component.ts
+++ BioSync.UI/src/app/edit-visitor/edit-visitor.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, Output, EventEmitter, OnInit} from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatButtonModule} from "@angular/material/button";
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-edit-visitor',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule, 
+    MatSelectModule],
   templateUrl: './edit-visitor.component.html',
   styleUrl: './edit-visitor.component.css'
 })
 export class EditVisitorComponent {
+  @Output() backToEditVisitor = new EventEmitter<void>();
+  visitorForm!: FormGroup;
 
+  labs: string[] = [
+    'DOST Laboratory',
+    'Aboitiz Laboratory',
+  ];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.visitorForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      visit: ['', Validators.required],
+      details: ['', [Validators.required]],
+      event: ['', Validators.required],
+      lab: ['', [Validators.required]],
+      time_in: ['', Validators.required],
+      time_out: ['', [Validators.required]],
+    });
+  }
+
+  cancelEditVisitor(): void {
+    this.backToEditVisitor.emit();
+  }
+
+  submit() {
+    console.log("Click Submit!");
+    return;
+  }
 }
