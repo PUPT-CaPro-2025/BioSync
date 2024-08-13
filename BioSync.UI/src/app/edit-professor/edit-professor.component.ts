5f4dbd9deeeb7462e1dea42a29d9986789e84a8a
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-13 18:01:40 +0800

    add 'edit-professor' component

---
+++ BioSync.UI/src/app/edit-professor/edit-professor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-professor',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-professor.component.html',
+  styleUrl: './edit-professor.component.css'
+})
+export class EditProfessorComponent {
+
+}
