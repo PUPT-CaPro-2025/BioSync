1c1998490ca4f1b68e7835f1e961dfc7daf2c07b
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-13 18:16:35 +0800

    Add 'edit-subject' component structure

--- BioSync.UI/src/app/edit-subject/edit-subject.component.ts
+++ BioSync.UI/src/app/edit-subject/edit-subject.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, Output, EventEmitter, OnInit} from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Subject} from "../../model/subject-model";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-edit-subject',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule],
   templateUrl: './edit-subject.component.html',
   styleUrl: './edit-subject.component.css'
 })
 export class EditSubjectComponent {
+  @Output() backToEditSubject = new EventEmitter<void>();
+  subjectForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.subjectForm = this.formBuilder.group({
+      code: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      description: ['', Validators.required]
+    });
+  }
+
+  cancelEditSubject(): void {
+    this.backToEditSubject.emit();
+  }
+
+  submit() {
+    console.log("Click Submit!");
+    return;
+  }
 }
