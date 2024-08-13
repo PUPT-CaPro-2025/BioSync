593775096e4cc0347134296f4298b1b206135530
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-13 18:09:09 +0800

    add 'edit-subject' component

---
+++ BioSync.UI/src/app/edit-subject/edit-subject.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-subject',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-subject.component.html',
+  styleUrl: './edit-subject.component.css'
+})
+export class EditSubjectComponent {
+
+}
