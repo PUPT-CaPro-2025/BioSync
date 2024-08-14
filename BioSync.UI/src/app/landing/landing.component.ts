96bfe690ecd6b655bcf71f9d0e3758de0e392f75
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-14 21:14:23 +0800

    Add landing component structure

--- BioSync.UI/src/app/landing/landing.component.ts
+++ BioSync.UI/src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  constructor(private router: Router) {}
 
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
