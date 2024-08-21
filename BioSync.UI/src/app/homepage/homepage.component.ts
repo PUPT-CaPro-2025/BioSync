0c0b4255004e49dc7ae7cd847910845bd6da1066
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-21 23:31:12 +0800

    Add temporary button for specific screen size

--- BioSync.UI/src/app/homepage/homepage.component.ts
+++ BioSync.UI/src/app/homepage/homepage.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
-import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { SidenavComponent } from '../sidenav/sidenav.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterOutlet, SidenavComponent, MatSidenavModule ],
+  imports: [RouterOutlet, SidenavComponent, MatSidenavModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
   showSideNav = true;
+  isMobile = false;
+
   private hideSideNavRoutes = [
-    '/login', 
-    '/admin-login', 
-    'faculty-login', 
-    'student-login', 
+    '/login',
+    '/admin-login',
+    'faculty-login',
+    'student-login',
     'visitor-log',
     'dashboard-student',
     'dashboard-professor'
@@ -25,12 +26,33 @@ export class HomepageComponent {
 
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
-      if(event instanceof NavigationEnd) {
+      if (event instanceof NavigationEnd) {
         this.showSideNav = !this.hideSideNavRoutes.some(
-          route => this.router.url.includes(route)
-        )
+          (route) => this.router.url.includes(route)
+        );
       }
-    })
+    });
+
+    this.checkScreenSize();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth <= 900;
+    if (!this.isMobile) {
+      this.showSideNav = true;
+    } else {
+      this.showSideNav = false;
+    }
+  }
+
+  onCloseSidenav() {
+    if (this.isMobile) {
+      this.showSideNav = false;
+    }
+  }
 }
