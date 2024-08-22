c016c32a41034e9fcc5be895b7ba977d6c3bc6c8
Jhean Galope <drakekhendrick@gmail.com>
2024-08-22 19:27:34 +0800

    refactor professor api to user api classified by roles

--- BioSync.API/src/main/java/com/example/biosyncapi/repository/ProfessorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.biosyncapi.repository;
-
-import com.example.biosyncapi.model.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-}
