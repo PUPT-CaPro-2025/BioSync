eecfa92f541f8ea1de39e58802c93cc9921e6743
Jhean Galope <drakekhendrick@gmail.com>
2024-08-22 21:34:59 +0800

    implement adding of professor on client side

--- BioSync.UI/src/app/add-professor/add-professor.component.ts
+++ BioSync.UI/src/app/add-professor/add-professor.component.ts
@@ -5,6 +5,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButtonModule} from "@angular/material/button";
 import { MatSelectModule } from '@angular/material/select';
+import {UserService} from "../../services/user.service";
+import {User} from "../../model/user.model";
+import {PromptOkayComponent} from "../prompt-okay/prompt-okay.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-add-professor',
@@ -16,14 +20,15 @@ import { MatSelectModule } from '@angular/material/select';
     ReactiveFormsModule,
     MatButtonModule,
     MatSelectModule],
+  providers: [UserService],
   templateUrl: './add-professor.component.html',
   styleUrl: './add-professor.component.css'
 })
 export class AddProfessorComponent implements OnInit{
   @Output() backToProfessor = new EventEmitter<void>();
 
-  //Temporary Suffixes
   allSuffix: string[] = [
+    'N/A',
     'Ph.D.',
     'Ed.D.',
     'D.Phil.',
@@ -39,7 +44,10 @@ export class AddProfessorComponent implements OnInit{
   professorForm!: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder) {}
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.initForm();
@@ -47,10 +55,10 @@ export class AddProfessorComponent implements OnInit{
 
   initForm(){
     this.professorForm = this.formBuilder.group({
-      code: ['', [Validators.required]],
-      first_name: ['', [Validators.required]],
-      last_name: ['', [Validators.required]],
-      middle_initial: ['', [Validators.required]],
+      usercode: ['', [Validators.required]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      middleName: ['', [Validators.required]],
       suffix: ['', [Validators.required]]
     });
   }
@@ -60,7 +68,30 @@ export class AddProfessorComponent implements OnInit{
   }
 
   submit(){
-    console.log("Click Submit!")
-    return;
+    if(!this.professorForm.valid) return;
+
+    const professorToCreate = {
+      ...this.professorForm.value,
+      role: 'FACULTY',
+      password: 'test123'
+    }
+
+    this.userService.createUser(professorToCreate).subscribe({
+      next: (userCreated: User) => {
+        if(!userCreated.id) return;
+        this.displaySuccess()
+      },
+      error: error => { console.log(error); }
+    });
+  }
+
+  displaySuccess() {
+    const dialogRef = this.dialog.open(PromptOkayComponent, {
+      width: '400px'
+    })
+
+    dialogRef.afterClosed().subscribe(result => {
+       this.backToProfessor.emit();
+    })
   }
 }
