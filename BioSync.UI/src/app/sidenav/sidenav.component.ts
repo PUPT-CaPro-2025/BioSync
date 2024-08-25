173d441af7f1a3a0231b52bc1382b2103d4d3c28
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-25 21:58:19 +0800

    add maintenance on sidenav

--- BioSync.UI/src/app/sidenav/sidenav.component.ts
+++ BioSync.UI/src/app/sidenav/sidenav.component.ts
@@ -8,29 +8,40 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink } from '@angular/router';
-import {LogoutService} from "../../services/auth/logout.service";
-import {CookieService} from "../../services/cookie.service";
-import {PromptConfirmComponent} from "../prompt-confirm/prompt-confirm.component";
-import {MatDialog} from "@angular/material/dialog";
+import { LogoutService } from '../../services/auth/logout.service';
+import { CookieService } from '../../services/cookie.service';
+import { PromptConfirmComponent } from '../prompt-confirm/prompt-confirm.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSelectModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatListModule, RouterLink, CommonModule, MatIconModule],
+  imports: [
+    MatSelectModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterLink,
+    CommonModule,
+    MatIconModule,
+  ],
   providers: [LogoutService, CookieService],
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
 })
 export class SidenavComponent implements OnInit {
   activeButton: string | null = 'dashboard';
-  isDropdownOpen = false;
+  isDropdownOpenStudent = false;
+  isDropdownOpenMaintenance = false;
+
   @Output() sidenavClose = new EventEmitter<void>();
 
   constructor(
     private router: Router,
     private logoutService: LogoutService,
     private cookieService: CookieService,
-    private dialog: MatDialog,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit() {
@@ -41,28 +52,44 @@ export class SidenavComponent implements OnInit {
   }
 
   onButtonClick(buttonName: string) {
-    if (buttonName !== 'student') {
+    if (buttonName !== 'student' && buttonName !== 'maintenance') {
       this.activeButton = buttonName;
       localStorage.setItem('activeButton', buttonName);
-      this.isDropdownOpen = false;
+      this.isDropdownOpenStudent = false;
+      this.isDropdownOpenMaintenance = false;
     }
   }
 
-  toggleDropdown(event: Event) {
+  toggleStudentDropdown(event: Event) {
     event.stopPropagation();
-    this.isDropdownOpen = !this.isDropdownOpen;
-    if (this.isDropdownOpen) {
+    this.isDropdownOpenStudent = !this.isDropdownOpenStudent;
+    if (this.isDropdownOpenStudent) {
       this.activeButton = 'student';
       localStorage.setItem('activeButton', 'student');
+      this.isDropdownOpenMaintenance = false;
+    }
+  }
+
+  toggleMaintenanceDropdown(event: Event) {
+    event.stopPropagation();
+    this.isDropdownOpenMaintenance = !this.isDropdownOpenMaintenance;
+    if (this.isDropdownOpenMaintenance) {
+      this.activeButton = 'maintenance';
+      localStorage.setItem('activeButton', 'maintenance');
+      this.isDropdownOpenStudent = false;
     }
   }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
-    this.isDropdownOpen = false;
+    this.isDropdownOpenStudent = false;
+    this.isDropdownOpenMaintenance = false;
     if (route === '/student' || route === '/attendance') {
       this.activeButton = 'student';
       localStorage.setItem('activeButton', 'student');
+    } else if (route === '/school-year' || route === '/program') {
+      this.activeButton = 'maintenance';
+      localStorage.setItem('activeButton', 'maintenance');
     } else {
       this.activeButton = route.split('/').pop() || '';
       localStorage.setItem('activeButton', this.activeButton);
@@ -76,9 +103,20 @@ export class SidenavComponent implements OnInit {
   @HostListener('document:click', ['$event'])
   closeDropdownOnClick(event: MouseEvent) {
     const target = event.target as HTMLElement;
-    if (this.isDropdownOpen && !target.closest('.menu-container') &&
-        !target.closest('.dropdown-content')) {
-      this.isDropdownOpen = false;
+    if (
+      this.isDropdownOpenStudent &&
+      !target.closest('.menu-container') &&
+      !target.closest('.dropdown-content')
+    ) {
+      this.isDropdownOpenStudent = false;
+      this.activeButton = null;
+      localStorage.removeItem('activeButton');
+    } else if (
+      this.isDropdownOpenMaintenance &&
+      !target.closest('.menu-container') &&
+      !target.closest('.dropdown-content')
+    ) {
+      this.isDropdownOpenMaintenance = false;
       this.activeButton = null;
       localStorage.removeItem('activeButton');
     }
@@ -88,26 +126,29 @@ export class SidenavComponent implements OnInit {
     const dialogRef = this.dialog.open(PromptConfirmComponent, {
       width: '400px',
       data: {
-        title: "Logout",
-        message: "Are you sure you want to log out of your account?",
-        action: "Logout"
-      }
+        title: 'Logout',
+        message: 'Are you sure you want to log out of your account?',
+        action: 'Logout',
+      },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.logout();
       }
     });
   }
 
-  logout(){
+  logout() {
     this.logoutService.logout().subscribe({
       next: () => {
-        this.cookieService.deleteCookie("authToken");
-        this.cookieService.deleteCookie("role");
+        this.cookieService.deleteCookie('authToken');
+        this.cookieService.deleteCookie('role');
         this.router.navigate(['/login']).then();
-      }, error: err => { console.log(err); }
-    })
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
