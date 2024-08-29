ce59a7896471e857bbdaff418b6e2a3d1ee5c0ad
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-29 16:06:46 +0800

    add 'add-laboratory' structure

--- BioSync.UI/src/app/add-laboratory/add-laboratory.component.ts
+++ BioSync.UI/src/app/add-laboratory/add-laboratory.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, Output, EventEmitter, OnInit} from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatButtonModule} from "@angular/material/button";
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-add-laboratory',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
   templateUrl: './add-laboratory.component.html',
   styleUrl: './add-laboratory.component.css'
 })
-export class AddLaboratoryComponent {
+export class AddLaboratoryComponent implements OnInit {
+  laboratoryForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.laboratoryForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      roomCode: ['', [Validators.required]],
+      capacity: ['', Validators.required]
+    });
+  }
+
+  submit(){
+    console.log("Submit");
+    return;
+  }
 }
