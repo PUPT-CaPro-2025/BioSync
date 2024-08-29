75be15a9d0bccf557f016e40532270e575834339
KRAyacocho <kylieross.ayacocho@gmail.com>
2024-08-29 20:41:14 +0800

    add 'school-year' component

---
+++ BioSync.UI/src/app/school-year/school-year.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-school-year',
+  standalone: true,
+  imports: [],
+  templateUrl: './school-year.component.html',
+  styleUrl: './school-year.component.css'
+})
+export class SchoolYearComponent {
+
+}
