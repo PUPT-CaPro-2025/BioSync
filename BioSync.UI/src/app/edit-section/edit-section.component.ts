fef3e3d449c384345ca5c586f34e3ba6e3c6bce4
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-29 14:31:32 +0800

    add 'edit-section' component structure

---
+++ BioSync.UI/src/app/edit-section/edit-section.component.ts
@@ -0,0 +1,66 @@
+import {Component, Output, EventEmitter, OnInit} from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatButtonModule} from "@angular/material/button";
+import { MatSelectModule } from '@angular/material/select';
+import { ProgramService } from '../../services/program.service';
+import { Program } from '../../model/program.model';
+
+@Component({
+  selector: 'app-edit-section',
+  standalone: true,
+  imports: [MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSelectModule
+  ],
+  providers: [ProgramService],
+  templateUrl: './edit-section.component.html',
+  styleUrl: './edit-section.component.css'
+})
+export class EditSectionComponent implements OnInit {
+  sectionForm!: FormGroup;
+
+  years: string[] = [
+    '1', '2', '3', '4', '5', 'Ladderized'
+  ];
+
+  sections: string[] =[
+    '1', '2', '3', '4', '5'
+  ];
+
+  programs: Program[] = [];
+
+  constructor(private formBuilder: FormBuilder, private programService: ProgramService) {}
+
+  ngOnInit() {
+    this.getAllPrograms();
+    this.initForm();
+  }
+
+  initForm(){
+    this.sectionForm = this.formBuilder.group({
+      program: ['', [Validators.required]],
+      year: ['', [Validators.required]],
+      section: ['', Validators.required]
+    });
+  }
+
+  getAllPrograms() {
+    this.programService.getAllPrograms().subscribe({
+      next: programs => {
+        this.programs = programs;
+      }
+    })
+  }
+
+  submit(){
+    console.log("Submit");
+    return;
+  }
+}
