a75d357d72fa00ce18a06c2d6f274f179e3fef65
Jhean Galope <drakekhendrick@gmail.com>
2024-08-30 14:54:55 +0800

    update request api handling

---
+++ BioSync.API/src/main/java/com/example/biosyncapi/config/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.biosyncapi.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ResponseEntity<String> handleNotFound(NoHandlerFoundException e) {
+        return new ResponseEntity<>("Resource not found", HttpStatus.NOT_FOUND);
+    }
+
+}
