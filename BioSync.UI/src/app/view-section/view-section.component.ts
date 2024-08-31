67372d72d411bddd7644340572dd9ee601c0b1c9
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-31 17:52:45 +0800

    add view section component

---
+++ BioSync.UI/src/app/view-section/view-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-section.component.html',
+  styleUrl: './view-section.component.css'
+})
+export class ViewSectionComponent {
+
+}
