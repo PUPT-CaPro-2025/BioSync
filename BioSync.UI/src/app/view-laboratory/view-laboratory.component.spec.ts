f8f5e7aea5ffdadf6b47e0dbbb2b9a765eb1fa74
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-31 16:19:07 +0800

    add 'view-laboratory' component

---
+++ BioSync.UI/src/app/view-laboratory/view-laboratory.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewLaboratoryComponent } from './view-laboratory.component';
+
+describe('ViewLaboratoryComponent', () => {
+  let component: ViewLaboratoryComponent;
+  let fixture: ComponentFixture<ViewLaboratoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ViewLaboratoryComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ViewLaboratoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
