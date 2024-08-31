4c205016a6ed5a104473882813df5765ec8fdedc
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-08-31 16:19:07 +0800

    add 'view-laboratory' component

---
+++ BioSync.UI/src/app/view-laboratory/view-laboratory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-laboratory',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-laboratory.component.html',
+  styleUrl: './view-laboratory.component.css'
+})
+export class ViewLaboratoryComponent {
+
+}
