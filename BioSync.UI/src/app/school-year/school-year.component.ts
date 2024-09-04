54eccea9c1babe0cef5f2d3f08134b2fb72113ed
Jhean Galope <drakekhendrick@gmail.com>
2024-09-04 21:33:00 +0800

    update school year display to be dynamic

--- BioSync.UI/src/app/school-year/school-year.component.ts
+++ BioSync.UI/src/app/school-year/school-year.component.ts
@@ -3,7 +3,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from "@angular/material/button";
 import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
@@ -14,16 +14,6 @@ import { EditSchoolYearComponent } from "../edit-school-year/edit-school-year.co
 import { AddSchoolYearComponent } from '../add-school-year/add-school-year.component';
 import { ViewSchoolYearComponent } from '../view-school-year/view-school-year.component';
 
-interface schoolYears {
-  year_start: string;
-  year_end: string;
-  first_sem_start: string;
-  first_sem_end: string;
-  second_sem_start: string;
-  second_sem_end: string;
-  summer_start: string;
-  summer_end: string;
-}
 
 @Component({
   selector: 'app-school-year',
@@ -37,7 +27,7 @@ interface schoolYears {
     MatButtonModule,
     MatSelectModule,
     CommonModule,
-    AddSchoolYearComponent, 
+    AddSchoolYearComponent,
     EditSchoolYearComponent,
     ViewSchoolYearComponent
   ],
@@ -45,26 +35,16 @@ interface schoolYears {
   templateUrl: './school-year.component.html',
   styleUrl: './school-year.component.css'
 })
-export class SchoolYearComponent {
+export class SchoolYearComponent implements OnInit{
       entries: string[] = [
         '10', '20', '30', '40', '50'
       ];
-    
+
       sorting: string[] = [
         'Alphabetical', 'Date'
       ];
 
-      schoolYear: schoolYears[] = [
-        { year_start: "2024", 
-          year_end: "2025", 
-          first_sem_start: "October 22, 2024",
-          first_sem_end: "February 23, 2025",
-          second_sem_start: "March 15, 2025",
-          second_sem_end: "July 13, 2025",
-          summer_start: "July 22, 2025",
-          summer_end: "September 25, 2025",
-        },
-      ];
+      schoolYear: SchoolYear[] = [];
 
       @Input() totalItems: number = 500;
       itemsPerPage: number = 10;
@@ -76,19 +56,43 @@ export class SchoolYearComponent {
       schoolYearToEdit!: SchoolYear;
 
       constructor(
-        private schoolYearService: SchoolYearService, 
+        private schoolYearService: SchoolYearService,
         private dialog: MatDialog) {}
 
-      get filteredSchoolYears(): schoolYears[] {
+    ngOnInit() {
+          this.getSchoolYears();
+    }
+
+     getSchoolYears(): void {
+          this.schoolYearService.getSchoolYears().subscribe({
+              next: (schoolYears: SchoolYear[]) => {
+                  this.schoolYear = schoolYears;
+              }
+          })
+     }
+
+    dateToReadable(isoDate: Date): string {
+        const date = new Date(isoDate);
+
+        const options: Intl.DateTimeFormatOptions = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+
+        return date.toLocaleDateString('en-US', options);
+    }
+
+      get filteredSchoolYears(): SchoolYear[] {
         const startIndex = (this.currentPage - 1) * this.itemsPerPage;
         const endIndex = startIndex + this.itemsPerPage;
         return this.schoolYear.slice(startIndex, endIndex);
       }
-    
+
       get pages(): number[] {
         return Array(this.totalPages).fill(0).map((_, i) => i + 1);
       }
-    
+
       getDisplayRange(): string {
         const start = (this.currentPage - 1) * this.itemsPerPage + 1;
         const end = Math.min(this.currentPage * this.itemsPerPage, this.totalItems);
@@ -98,7 +102,7 @@ export class SchoolYearComponent {
       onPageChange(): void {
         // Handle page change logic here
       }
-    
+
       onItemsPerPageChange(): void {
         this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
         if (this.currentPage > this.totalPages) {
@@ -106,14 +110,14 @@ export class SchoolYearComponent {
         }
         this.onPageChange();
       }
-    
+
       previousPage(): void {
         if (this.currentPage > 1) {
           this.currentPage--;
           this.onPageChange();
         }
       }
-    
+
       nextPage(): void {
         if (this.currentPage < this.totalPages) {
           this.currentPage++;
@@ -124,15 +128,15 @@ export class SchoolYearComponent {
       toggleAddSchoolYear(): void {
         this.isAddSchoolYear = !this.isAddSchoolYear;
       }
-    
+
       handleBackToSchoolYear(): void {
         this.isAddSchoolYear = false;
       }
-    
+
       toggleEditSchoolYear(): void {
         this.isEditSchoolYear = !this.isEditSchoolYear;
       }
-    
+
       handleBackToEditSchoolYear(): void {
         this.isEditSchoolYear = false;
       }
@@ -140,7 +144,7 @@ export class SchoolYearComponent {
       toggleViewSchoolYear(): void {
         this.isViewSchoolYear = !this.isViewSchoolYear;
       }
-    
+
       handleBackToViewSchoolYear(): void {
         this.isViewSchoolYear = false;
       }
