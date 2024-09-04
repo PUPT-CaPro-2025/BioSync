9507ee056401e0bf02c3299e7f051c03fe0ca466
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-09-04 16:24:09 +0800

    add student-schedule component

---
+++ BioSync.UI/src/app/student-schedule/student-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-schedule.component.html',
+  styleUrl: './student-schedule.component.css'
+})
+export class StudentScheduleComponent {
+
+}
