732bcfb280c1cf6487558fea1aa7c89f2bf954fb
Jhean Galope <drakekhendrick@gmail.com>
2024-09-05 00:03:35 +0800

    setup display of sections on client

--- BioSync.UI/src/app/section/section.component.ts
+++ BioSync.UI/src/app/section/section.component.ts
@@ -3,22 +3,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from "@angular/material/button";
 import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { Section } from '../../model/section.model';
-import { Semester } from '../../model/semester.model'; 
 import { SectionService } from '../../services/section.service';
 import { AddSectionComponent } from '../add-section/add-section.component';
-import { EditSectionComponent } from '../edit-section/edit-section.component';
-import { ViewSectionComponent } from '../view-section/view-section.component';
+import {MatDialog} from "@angular/material/dialog";
 
-interface sections {
-  program: string;
-  year: string;
-  section: string;
-}
 
 @Component({
   selector: 'app-section',
@@ -33,14 +26,12 @@ interface sections {
     MatSelectModule,
     CommonModule,
     AddSectionComponent,
-    EditSectionComponent,
-    ViewSectionComponent
   ],
   providers: [SectionService],
   templateUrl: './section.component.html',
   styleUrl: './section.component.css'
 })
-export class SectionComponent {
+export class SectionComponent implements OnInit{
   entries: string[] = [
     '10', '20', '30', '40', '50'
   ];
@@ -49,16 +40,7 @@ export class SectionComponent {
     'Alphabetical', 'Date'
   ];
 
-  section: sections[] = [
-    { program: "BSIT", year: "1", section: "1" },
-    { program: "BSIT", year: "2", section: "1" },
-    { program: "BSIT", year: "3", section: "1" },
-    { program: "BSIT", year: "4", section: "1" },
-    { program: "DIT", year: "1", section: "1" },
-    { program: "DIT", year: "2", section: "1" },
-    { program: "DIT", year: "3", section: "1" },
-    { program: "DIT", year: "4", section: "1" },
-  ];
+  section: Section[] = [];
 
   @Input() totalItems: number = 500;
   itemsPerPage: number = 10;
@@ -69,7 +51,24 @@ export class SectionComponent {
   isViewSection: boolean = false;
   sectionToEdit!: Section;
 
-  get filteredSections(): sections[] {
+  constructor(
+    private dialog: MatDialog,
+    private sectionService: SectionService,
+  ) {}
+
+  ngOnInit() {
+    this.getSections();
+  }
+
+  getSections(){
+    this.sectionService.getSections().subscribe({
+      next: (sections: Section[]) => {
+        this.section = sections;
+      }
+    })
+  }
+
+  get filteredSections(): Section[] {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
     return this.section.slice(startIndex, endIndex);
