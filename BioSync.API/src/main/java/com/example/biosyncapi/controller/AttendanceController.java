f7d30b03bde033e6d8ba504e9ee57868fa15cd55
Jhean Galope <drakekhendrick@gmail.com>
2024-09-15 16:17:18 +0800

    add controller for student check-in - partial

--- BioSync.API/src/main/java/com/example/biosyncapi/controller/AttendanceController.java
+++ BioSync.API/src/main/java/com/example/biosyncapi/controller/AttendanceController.java
@@ -1,5 +1,6 @@
 package com.example.biosyncapi.controller;
 
+import com.example.biosyncapi.model.User;
 import com.example.biosyncapi.service.FingerprintService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/attendance")
@@ -32,4 +34,16 @@ public class AttendanceController {
         return ResponseEntity.status(401).body("Fingerprint verification failed.");
     }
 
+    @PostMapping("/student/check-in")
+    public ResponseEntity<?> verifyStudentTimeInAttendance(
+            @RequestParam("sectionId") Long sectionId,
+            @RequestParam("fingerprint") MultipartFile fingerprint
+    ) throws IOException {
+
+        User student = fingerprintService.verifyStudentFingerprintForAttendance(sectionId,fingerprint);
+
+        if (student == null) return ResponseEntity.status(401).body("Fingerprint verification failed.");
+
+        return ResponseEntity.ok().body(Map.of("message", "Fingerprint verified.", "student", student));
+    }
 }
