5cc3a89a94c6d9d29a16c88d9b89ea5355a04a48
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-09-15 13:54:49 +0800

    add faculty-schedule component

---
+++ BioSync.UI/src/app/faculty-schedule/faculty-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './faculty-schedule.component.html',
+  styleUrl: './faculty-schedule.component.css'
+})
+export class FacultyScheduleComponent {
+
+}
