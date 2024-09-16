ff4707b6afed0a5055fd8c62fc08bcd900dcffbf
Jhean <drakekhendrick@gmail.com>
2024-09-17 04:49:40 +0800

    initialize sdk to start attendance component

--- BioSync.UI/src/app/start-attendance/start-attendance.component.ts
+++ BioSync.UI/src/app/start-attendance/start-attendance.component.ts
@@ -3,6 +3,7 @@ import {Schedule} from "../../model/schedule.model";
 import {ActivatedRoute} from "@angular/router";
 import {ScheduleService} from "../../services/schedule.service";
 import {MatToolbar} from "@angular/material/toolbar";
+import {FingerprintService} from "../../services/fingerprint.service";
 
 @Component({
   selector: 'app-start-attendance',
@@ -10,16 +11,19 @@ import {MatToolbar} from "@angular/material/toolbar";
   imports: [
     MatToolbar
   ],
-  providers: [ScheduleService],
+  providers: [ScheduleService, FingerprintService],
   templateUrl: './start-attendance.component.html',
   styleUrl: './start-attendance.component.css'
 })
 export class StartAttendanceComponent implements OnInit{
+  selectedProfessorId!: number;
   selectedSchedule!: Schedule;
+  fingerprintImageSrc!: Blob;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private scheduleService: ScheduleService
+    private scheduleService: ScheduleService,
+    private fingerprintService: FingerprintService
   ) {}
 
   ngOnInit() {
@@ -29,12 +33,23 @@ export class StartAttendanceComponent implements OnInit{
         this.getScheduleDetails(scheduleId);
       }
     })
+    this.fingerprintService.loadSDK();
+
+    this.fingerprintService.getImageSrc().subscribe({
+      next: (src) => {
+        if (src) {
+          this.fingerprintImageSrc = this.base64ToBlob(src, 'image/png');
+          this.submit();
+        }
+      }
+    });
   }
 
   getScheduleDetails(scheduleId: number){
     this.scheduleService.getScheduleById(scheduleId).subscribe({
       next: value => {
         this.selectedSchedule = value;
+        this.selectedProfessorId = value.professor?.id!;
       }
     })
   }
@@ -46,5 +61,24 @@ export class StartAttendanceComponent implements OnInit{
   getDay(date: string){
     return this.scheduleService.getDay(date);
   }
+
+  private base64ToBlob(base64: string, contentType: string) {
+    return this.fingerprintService.base64ToBlob(base64, contentType);
+  }
+
+  submit(){
+    const formData = new FormData();
+
+    formData.append('userId', this.selectedProfessorId.toString());
+    formData.append('fingerprint', this.fingerprintImageSrc, 'fingerprint.png')
+
+
+    for (let pair of (formData as any).entries()) {
+      console.log(pair[0] + ':', pair[1]);
+    }
+
+  }
+
+
 
 }
