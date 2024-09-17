8e55c9baaae6b83e00a4e17d58befcd7d8a19389
Jhean <drakekhendrick@gmail.com>
2024-09-17 14:07:45 +0800

    update list projection

--- BioSync.UI/src/app/schedule-list/schedule-list.component.ts
+++ BioSync.UI/src/app/schedule-list/schedule-list.component.ts
@@ -38,25 +38,36 @@ export class ScheduleListComponent implements OnInit {
     });
   }
 
-  getSchedulesByRecurrenceId(recurrenceId: string){
+  getSchedulesByRecurrenceId(recurrenceId: string) {
     this.scheduleService.getSchedulesByRecurrenceId(recurrenceId).subscribe({
       next: (schedules: Schedule[]) => {
         const today = this.today;
 
-        const futureSchedules = schedules.filter(schedule => new Date(schedule.scheduleDate).getTime() >= today);
-        const pastSchedules = schedules.filter(schedule => new Date(schedule.scheduleDate).getTime() < today);
+        const futureSchedules = schedules.filter(schedule =>
+          new Date(schedule.scheduleDate).getTime() >= today && !schedule.hasFinished
+        );
+
+        const finishedSchedules = schedules.filter(schedule =>
+          schedule.hasFinished && new Date(schedule.scheduleDate).getTime() >= today
+        );
+
+        const pastSchedules = schedules.filter(schedule =>
+          new Date(schedule.scheduleDate).getTime() < today && !schedule.hasFinished
+        );
 
         futureSchedules.sort((a, b) => new Date(a.scheduleDate).getTime() - new Date(b.scheduleDate).getTime());
+        finishedSchedules.sort((a, b) => new Date(a.scheduleDate).getTime() - new Date(b.scheduleDate).getTime());
         pastSchedules.sort((a, b) => new Date(b.scheduleDate).getTime() - new Date(a.scheduleDate).getTime());
 
-        this.schedules = [...futureSchedules, ...pastSchedules];
+        this.schedules = [...futureSchedules, ...finishedSchedules, ...pastSchedules];
 
         this.schedule = this.schedules[0];
       }
-    })
+    });
   }
 
   isScheduledForFutureOrToday(schedule: Schedule): boolean {
+    console.log(schedule.hasFinished);
     return (new Date(schedule.scheduleDate).getTime() >= this.today) && !schedule.hasFinished;
   }
 
