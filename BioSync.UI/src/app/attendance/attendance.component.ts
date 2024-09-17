0da16604aadd2b79d53bfc023f76948286a14ad5
Jhean <drakekhendrick@gmail.com>
2024-09-18 02:02:13 +0800

    setup attendance component on client

--- BioSync.UI/src/app/attendance/attendance.component.ts
+++ BioSync.UI/src/app/attendance/attendance.component.ts
@@ -1,31 +1,32 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
-
-interface studentSchedule {
-  student_number: string;
-  last_name: string;
-  first_name: string;
-  time_in: string;
-  time_out: string;
-  subject_name: string;
-  schedule_date: string;
-  section: string;
-  start_time: string;
-  end_time: string;
-}
+import {AddScheduleComponent} from "../add-schedule/add-schedule.component";
+import {EditScheduleComponent} from "../edit-schedule/edit-schedule.component";
+import {SchoolYear} from "../../model/school.year.model";
+import {Schedule} from "../../model/schedule.model";
+import {ScheduleService} from "../../services/schedule.service";
+import {MatDialog} from "@angular/material/dialog";
+import {SchoolYearService} from "../../services/school.year.service";
+import {Router} from "@angular/router";
+import {CryptoService} from "../../services/crypto.service";
+import {CookieService} from "../../services/cookie.service";
+import {UserService} from "../../services/user.service";
+import {PromptConfirmComponent} from "../prompt-confirm/prompt-confirm.component";
+import {User} from "../../model/user.model";
 
 @Component({
   selector: 'app-attendance',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, CommonModule, FormsModule, MatSelectModule],
+  imports: [MatToolbarModule, MatIconModule, CommonModule, FormsModule, AddScheduleComponent, MatSelectModule, EditScheduleComponent],
+  providers: [ScheduleService, SchoolYearService, UserService, CookieService, CryptoService],
   templateUrl: './attendance.component.html',
   styleUrl: './attendance.component.css'
 })
-export class AttendanceComponent {
+export class AttendanceComponent implements OnInit{
   entries: string[] = [
     '10', '20', '30', '40', '50'
   ];
@@ -34,26 +35,109 @@ export class AttendanceComponent {
     'Subject Code', 'Alphabetical', 'Date'
   ];
 
-  yearSemesters: string[] = [
-    'School Year 2324 - First Semester', 'School Year 2324 - Second Semester', 'School Year 2324 - Summer'
-  ];
+  academicYears: SchoolYear[] = [];
+  selectedAcademicYear: number | undefined;
 
-  selectedYearSem = 'School Year 2324 - Summer';
 
-    //Temporary data
-  student: studentSchedule[] = [
-    { student_number: "2021-00123-TG-0", last_name: "Doe", first_name:"John", time_in: "7:30 AM", time_out: "10:30 AM", subject_name: "Fundamentals of Computing", schedule_date: "09/08/2024", section: "BSIT 3-1", start_time: "7:30 AM", end_time: "10:30 AM"},
-    { student_number: "2022-00456-TG-0", last_name: "Ramirez", first_name:"Lemmuel", time_in: "01:00 PM", time_out: "05:00 PM", subject_name: "Computer Programming I", schedule_date: "10/06/2024", section: "BSIT 2-1", start_time: "01:00 PM", end_time: "05:00 PM"},
-    { student_number: "2022-00789-TG-0", last_name: "Kinoshita", first_name:"Aaron", time_in: "01:00 PM", time_out: "01:00 PM", subject_name: "Information Technology Fundamentals", schedule_date: "24/07/2024", section: "BSIT 2-1", start_time: "01:00 PM", end_time: "05:00 PM"},
+  semesters: string[] = [
+    'First Semester', 'Second Semester', 'Summer Semester',
   ];
+  selectedSemester = 1;
+
+  schedules: Schedule[] = [];
+  scheduleContainer: Schedule[] = [];
 
   @Input() totalItems: number = 500;
   itemsPerPage: number = 10;
   currentPage: number = 1;
   totalPages: number = Math.ceil(this.totalItems / this.itemsPerPage);
-  isAddSchedule: boolean = false;
-  isEditSchedule: boolean = false;
-  isViewSchedule: boolean = false;
+  userId!: number;
+
+  constructor(
+    private scheduleService: ScheduleService,
+    private dialog: MatDialog,
+    private schoolYearService: SchoolYearService,
+    private router : Router,
+    private cryptoService: CryptoService,
+    private cookieService: CookieService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    if(this.getRole() === "ADMIN"){
+      this.getAllSchedules();
+    } else if (this.getRole() === "FACULTY"){
+      this.getUserId();
+      this.getFacultySchedule(this.userId);
+    } else {
+      this.getUserId();
+      this.getSectionId(this.userId);
+    }
+    this.getAcademicYears();
+  }
+
+  getAllSchedules() {
+    this.scheduleService.getAllSchedules().subscribe({
+      next: (schedules) => {
+        this.schedules = schedules.filter(schedule => schedule.hasFinished);
+        this.scheduleContainer = schedules.filter(schedule => schedule.hasFinished);
+        this.sortSchedulesById(this.schedules);
+        //this.filteredRepeatedSchedules();
+        this.setLatestSchoolYear();
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  getUserId(){
+    const encryptedUserId = decodeURIComponent(this.cookieService.getCookie("user_id")!);
+    this.userId = +this.cryptoService.decrypt(encryptedUserId);
+  }
+
+  getRole(){
+    return this.cryptoService.decrypt(
+      decodeURIComponent(this.cookieService.getCookie("role")!));
+  }
+
+  getFacultySchedule(facultyId: number) {
+    this.scheduleService.getAllSchedulesByProfessorId(facultyId).subscribe({
+      next: (schedules: Schedule[]) => {
+        this.schedules = schedules.filter(schedule => schedule.hasFinished);
+        this.scheduleContainer = schedules;
+        this.filteredRepeatedSchedules();
+        this.sortSchedulesById(this.schedules);
+        this.setLatestSchoolYear();
+      }
+    })
+  }
+
+  setLatestSchoolYear() {
+    if (this.schedules && this.schedules.length > 0) {
+      const latestSchedule = this.schedules[this.schedules.length - 1];
+      this.selectedAcademicYear = latestSchedule.schoolYear?.id;
+      console.log(this.selectedAcademicYear);
+    }
+  }
+
+  sortSchedulesById(schedules: Schedule[]): Schedule[] {
+    return schedules.sort((a, b) => b.id - a.id);
+  }
+
+  convertTimeFormat(time: string): string {
+    return this.scheduleService.convertTimeFormat(time);
+  }
+
+  getDayOfWeek(date: string | Date): string {
+    return this.scheduleService.getDayOfWeek(date);
+  }
+
+  getAcademicYears() {
+    this.schoolYearService.getSchoolYears().subscribe({
+      next: (academicYears: SchoolYear[]) => {
+        this.academicYears = academicYears;
+      }
+    })
+  }
 
   get pages(): number[] {
     return Array(this.totalPages).fill(0).map((_, i) => i + 1);
@@ -65,10 +149,10 @@ export class AttendanceComponent {
     return `${start}-${end}`;
   }
 
-  get filteredAttendance(): studentSchedule[] {
+  get filteredSchedules(): Schedule[] {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
-    return this.student.slice(startIndex, endIndex);
+    return this.schedules.slice(startIndex, endIndex);
   }
 
   onPageChange(): void {
@@ -97,27 +181,55 @@ export class AttendanceComponent {
     }
   }
 
-  toggleAddSchedule(): void {
-    this.isAddSchedule = !this.isAddSchedule;
+  filteredRepeatedSchedules(): void {
+    const filteredSchedules: Schedule[] = [];
+    const recurrenceIdStorage: string[] = [];
+    this.schedules.forEach(schedule => {
+      if (schedule.recurrenceId != null) {
+        if(!recurrenceIdStorage.includes(schedule.recurrenceId)) {
+          recurrenceIdStorage.push(schedule.recurrenceId);
+          filteredSchedules.push(schedule);
+        }
+      } else {
+        filteredSchedules.push(schedule);
+      }
+    })
+
+    this.schedules = filteredSchedules;
   }
 
-  handleBackToSchedule(): void {
-    this.isAddSchedule = false;
+  onFilterChange() {
+    this.schedules = this.scheduleContainer.filter(
+      schedule => schedule.schoolYear?.id === this.selectedAcademicYear
+        && schedule.semester?.id === this.selectedSemester
+    )
+    this.filteredRepeatedSchedules();
+    this.sortSchedulesById(this.schedules);
   }
 
-  toggleEditSchedule(): void {
-    this.isEditSchedule = !this.isEditSchedule;
+  toggleViewAttendance(schedule: Schedule) {
+    this.router.navigate(["/view/attendance", schedule.id]).then();
   }
 
-  handleEditBackToSchedule(): void {
-    this.isEditSchedule = false;
+  getSectionId(userId: number) {
+    this.userService.getUserById(userId).subscribe({
+      next: (user: User) => {
+        if(!user.id) return;
+        this.getStudentSchedules(+user.section?.id!);
+      }
+    })
   }
 
-  toggleViewSchedule(): void {
-    this.isViewSchedule = !this.isViewSchedule;
-  }
-
-  handleViewBackToSchedule(): void {
-    this.isViewSchedule = false;
+  getStudentSchedules(sectionId: number){
+    this.scheduleService.getAllSchedulesBySectionId(sectionId).subscribe({
+      next: (schedules: Schedule[]) => {
+        console.log(schedules)
+        this.schedules = schedules;
+        this.scheduleContainer = schedules;
+        this.filteredRepeatedSchedules();
+        this.sortSchedulesById(this.schedules);
+        this.setLatestSchoolYear();
+      }
+    })
   }
 }
