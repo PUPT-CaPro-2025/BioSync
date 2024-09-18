aa553ae758e01b788ef1c4979b73b1a29776f088
Jhean <drakekhendrick@gmail.com>
2024-09-18 11:43:53 +0800

    update add professor component

--- BioSync.UI/src/app/add-professor/add-professor.component.ts
+++ BioSync.UI/src/app/add-professor/add-professor.component.ts
@@ -2,7 +2,7 @@ import {Component, Output, EventEmitter, OnInit} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButtonModule} from "@angular/material/button";
 import { MatSelectModule } from '@angular/material/select';
 import {UserService} from "../../services/user.service";
