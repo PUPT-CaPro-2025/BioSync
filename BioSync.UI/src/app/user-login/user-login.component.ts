7caff279ad5cd8dffe97db6c3f5d02758890bd0c
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-09-24 21:19:05 +0800

    import Mat Icon

--- BioSync.UI/src/app/user-login/user-login.component.ts
+++ BioSync.UI/src/app/user-login/user-login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
