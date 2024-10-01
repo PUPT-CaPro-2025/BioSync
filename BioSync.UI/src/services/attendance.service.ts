93c9d2150f8e374c26ec7fe5359a266feab74b39
Jhean Galope <drakekhendrick@gmail.com>
2024-10-02 00:18:24 +0800

    get profile url on student log

--- BioSync.UI/src/services/attendance.service.ts
+++ BioSync.UI/src/services/attendance.service.ts
@@ -1,42 +1,48 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {environment} from "../../environment/app.setting";
-import {CookieService} from "./cookie.service";
-import {Attendance} from "../model/attendance.model";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environment/app.setting';
+import { CookieService } from './cookie.service';
+import { Attendance } from '../model/attendance.model';
 
 @Injectable()
 export class AttendanceService {
   url = `${environment.apiUrl}/api/v1/attendance`;
-  accessToken = this.cookieService.getCookie("authToken");
-  headers = new HttpHeaders().set('Authorization', `Bearer ${this.accessToken}`);
+  accessToken = this.cookieService.getCookie('authToken');
+  headers = new HttpHeaders().set(
+    'Authorization',
+    `Bearer ${this.accessToken}`,
+  );
 
-  constructor(private http: HttpClient, private cookieService: CookieService) { }
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
+  ) {}
 
-  getAttendance(){
+  getAttendance() {
     return this.http.get<Attendance[]>(`${this.url}`, {
       headers: this.headers,
-      withCredentials: true
-    })
+      withCredentials: true,
+    });
   }
 
-  getAttendanceByScheduleId(scheduleId: number){
+  getAttendanceByScheduleId(scheduleId: number) {
     return this.http.get<Attendance[]>(`${this.url}/schedule/${scheduleId}`, {
       headers: this.headers,
-      withCredentials: true
-    })
+      withCredentials: true,
+    });
   }
 
-  getPresentCount(userId: number){
+  getPresentCount(userId: number) {
     return this.http.get<number>(`${this.url}/count/present/${userId}`, {
       headers: this.headers,
-      withCredentials: true
-    })
+      withCredentials: true,
+    });
   }
 
-  getAbsentCount(userId: number){
+  getAbsentCount(userId: number) {
     return this.http.get<number>(`${this.url}/count/absent/${userId}`, {
       headers: this.headers,
-      withCredentials: true
-    })
+      withCredentials: true,
+    });
   }
 }
