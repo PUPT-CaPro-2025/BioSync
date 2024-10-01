0f18aff5db47d6bebb415661a8837c3057fac5fd
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-10-01 20:55:44 +0800

    add mat icon module

--- BioSync.UI/src/app/password-forgot/password-forgot.component.ts
+++ BioSync.UI/src/app/password-forgot/password-forgot.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-password-forgot',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './password-forgot.component.html',
   styleUrl: './password-forgot.component.css'
 })
