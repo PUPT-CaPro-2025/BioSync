b0512b029889a5ca36f4a5970f82ee74346a1664
Jhean Galope <drakekhendrick@gmail.com>
2024-10-04 02:11:35 +0800

    add csv template link on env

--- BioSync.UI/environment/app.setting.ts
+++ BioSync.UI/environment/app.setting.ts
@@ -1,4 +1,5 @@
 export const environment = {
   apiUrl: 'http://localhost:8080',
-  key: 'helloworld'
+  key: 'helloworld',
+  templateLink: 'https://docs.google.com/spreadsheets/d/1B9wCMAxM-AHHXpqnVzVzPuPKa1S-z5tDo89L6H3bTPE/edit?usp=sharing'
 };
