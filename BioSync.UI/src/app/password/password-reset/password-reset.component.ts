a60e32008d628fec15daedfd042cf745c881a30b
Jhean Galope <drakekhendrick@gmail.com>
2024-10-05 01:43:28 +0800

    handle password reset on component

--- BioSync.UI/src/app/password/password-reset/password-reset.component.ts
+++ BioSync.UI/src/app/password/password-reset/password-reset.component.ts
@@ -1,26 +1,109 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {ActivatedRoute, Router} from "@angular/router";
+import {PasswordService} from "../../../services/password.service";
+import {MatDialog} from "@angular/material/dialog";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {PromptOkayComponent} from "../../prompt/prompt-okay/prompt-okay.component";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-password-reset',
   standalone: true,
-  imports: [MatIconModule, ReactiveFormsModule, CommonModule],
+  imports: [MatIconModule, ReactiveFormsModule, CommonModule, MatProgressSpinner, MatInput],
+  providers: [PasswordService],
   templateUrl: './password-reset.component.html',
-  styleUrl: './password-reset.component.css'
+  styleUrl: './password-reset.component.css',
 })
-export class PasswordResetComponent {
-    resetPasswordForm: FormGroup;
+export class PasswordResetComponent implements OnInit {
+  resetPasswordForm!: FormGroup;
+  resetToken!: string;
+  hasSubmitted = false;
+  errorMessage: string | null = null;
 
-    constructor(private formBuilder: FormBuilder) {
-        this.resetPasswordForm = this.formBuilder.group({
-            newPassword: ['', Validators.required],
-            confirmPassword: ['', Validators.required]
-        });
+  constructor(
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private passwordService: PasswordService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit() {
+    this.initForm();
+    this.getToken();
+  }
+
+  initForm() {
+    this.resetPasswordForm = this.formBuilder.group({
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  getToken() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      const token = params['token'];
+      if (token) {
+        this.resetToken = token;
+      } else {
+        this.router.navigate(['/login']).then();
+      }
+    });
+  }
+
+  submit(){
+    if(this.resetPasswordForm.invalid) {
+      this.errorMessage = "Please enter valid values";
+      return;
     }
 
-    get isFormValid() {
-        return this.resetPasswordForm.valid;
+    const { newPassword, confirmPassword } = this.resetPasswordForm.value;
+
+    if(newPassword !== confirmPassword) {
+      this.errorMessage = "Passwords Do Not Match";
+      return;
     }
+
+    this.hasSubmitted = true;
+
+
+    this.passwordService.resetPassword(this.resetToken, confirmPassword).subscribe({
+      next: () => {
+        this.openConfirmDialog(true);
+        this.hasSubmitted = false;
+      },
+      error: () => {
+        this.openConfirmDialog(false);
+        this.hasSubmitted = false;
+      }
+    })
+  }
+
+  openConfirmDialog(confirm: boolean) {
+    this.dialog.open(PromptOkayComponent, {
+      width: '400px',
+      data: {
+        title: confirm ? 'Password Updated!' : 'Something Went Wrong',
+        message: confirm ? 'Password has been changed successfully.' : 'Please try again in a moment.',
+      }
+    })
+  }
+
+  setMessageToNull(){
+    if(this.errorMessage != null){
+      this.errorMessage = null;
+    }
+  }
+
+  get isFormValid() {
+    return this.resetPasswordForm.valid;
+  }
 }
