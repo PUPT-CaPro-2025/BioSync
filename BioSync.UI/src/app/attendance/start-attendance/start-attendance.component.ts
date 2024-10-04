89f331164956df69160de883f508fcd9ebcc93a5
Jhean <drakekhendrick@gmail.com>
2024-10-04 14:07:02 +0800

    update start attendance component

--- BioSync.UI/src/app/attendance/start-attendance/start-attendance.component.ts
+++ BioSync.UI/src/app/attendance/start-attendance/start-attendance.component.ts
@@ -47,15 +47,17 @@ export class StartAttendanceComponent implements OnInit {
     private router: Router,
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.activatedRoute.paramMap.subscribe({
       next: (params) => {
         const scheduleId = +params.get('id')!;
         this.getScheduleDetails(scheduleId);
       },
     });
-    this.hasFingerprintScanner = this.sdkService.loadSDK();
-    console.log(this.hasFingerprintScanner);
+    this.hasFingerprintScanner = await this.sdkService.loadSDK();
+    setTimeout(() => {
+      console.log("check " +  this.sdkService.loadSDK());
+    }, 1000)
     this.hasBarcodeScanner = false; // TODO: initiate barcode reader
     this.hasDevice = this.hasBarcodeScanner || this.hasFingerprintScanner;
 
