cb0149bb4b089561e6804c1cf2d5a7fb9ead4f2f
Jhean Galope <drakekhendrick@gmail.com>
2024-10-04 12:09:03 +0800

    add find by email in user repository

--- BioSync.API/src/main/java/com/example/biosyncapi/user/UserRepository.java
+++ BioSync.API/src/main/java/com/example/biosyncapi/user/UserRepository.java
@@ -7,13 +7,17 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsercode(String usercode);
-    boolean existsByUsercode(String usercode);
-    List<User> findBySectionId(Long sectionId);
+  Optional<User> findByUsercode(String usercode);
 
-    @Query("SELECT u FROM User u WHERE u.id = :userId")
-    User findByUserId(Long userId);
+  boolean existsByUsercode(String usercode);
 
-    @Query("SELECT u FROM User u WHERE u.role = :role")
-    List<User> getUsersByRole(Role role);
+  List<User> findBySectionId(Long sectionId);
+
+  User findByEmail(String email);
+
+  @Query("SELECT u FROM User u WHERE u.id = :userId")
+  User findByUserId(Long userId);
+
+  @Query("SELECT u FROM User u WHERE u.role = :role")
+  List<User> getUsersByRole(Role role);
 }
