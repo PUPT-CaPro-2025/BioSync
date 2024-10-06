c175e47238e6b86d4477a54aab0939cd6a62f745
Jhean Galope <drakekhendrick@gmail.com>
2024-10-07 03:34:36 +0800

    fix: resolve user deletion bug that prevented cascade deletions

--- BioSync.API/src/main/java/com/example/biosyncapi/user/UserRepository.java
+++ BioSync.API/src/main/java/com/example/biosyncapi/user/UserRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsercode(String usercode);
 
+  User findById(long id);
+
   boolean existsByUsercode(String usercode);
 
   boolean existsByEmail(String email);
