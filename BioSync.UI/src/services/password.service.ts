92071394122b7e93cce885e2013ecc0a9a80d12e
Jhean Galope <drakekhendrick@gmail.com>
2024-10-07 01:30:47 +0800

    add headers on reset service

--- BioSync.UI/src/services/password.service.ts
+++ BioSync.UI/src/services/password.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@angular/core';
-import {environment} from "../../environment/app.setting";
-import {HttpClient} from "@angular/common/http";
+import { environment } from '../../environment/app.setting';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class PasswordService {
   url = `${environment.apiUrl}/api/v1/password`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   forgotPassword(email: string) {
-    return this.http.post(`${this.url}/forgot?email=${email}`, {}, {
-      withCredentials: true,
-      responseType: 'text' as 'json',
-    })
+    return this.http.post(
+      `${this.url}/forgot?email=${email}`,
+      {},
+      {
+        withCredentials: true,
+        responseType: 'text' as 'json',
+      },
+    );
   }
 
   resetPassword(token: string, password: string) {
-    return this.http.post(`${this.url}/reset`, { token, password }, {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+    const body = new URLSearchParams();
+    body.set('token', token);
+    body.set('newPassword', password);
+
+    return this.http.post(`${this.url}/reset`, body.toString(), {
+      headers: headers,
       withCredentials: true,
+      responseType: 'text' as 'json',
     });
   }
 }
