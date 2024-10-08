0d743f4185a6d0ceadbb9e34b740f5f16fd900f0
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-10-08 16:09:54 +0800

    add conditions for request schedule

--- BioSync.UI/src/app/schedule/schedule.component.ts
+++ BioSync.UI/src/app/schedule/schedule.component.ts
@@ -18,12 +18,26 @@ import {CookieService} from "../../services/cookie.service";
 import {User} from "../../model/user.model";
 import {UserService} from "../../services/user.service";
 import jsPDF from "jspdf";
+import { RequestScheduleComponent } from './request-schedule/request-schedule.component';
 
 @Component({
   selector: 'app-schedule',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, CommonModule, FormsModule, AddScheduleComponent, MatSelectModule, EditScheduleComponent],
-  providers: [ScheduleService, SchoolYearService, UserService, CookieService, CryptoService],
+  imports: [MatToolbarModule, 
+    MatIconModule, 
+    CommonModule, 
+    FormsModule, 
+    AddScheduleComponent, 
+    MatSelectModule, 
+    EditScheduleComponent, 
+    RequestScheduleComponent
+  ],
+  providers: [ScheduleService, 
+    SchoolYearService, 
+    UserService, 
+    CookieService, 
+    CryptoService
+  ],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.css',
 })
@@ -55,10 +69,13 @@ export class ScheduleComponent implements OnInit{
   totalPages: number = Math.ceil(this.totalItems / this.itemsPerPage);
   isOneAddSchedule: boolean = false;
   isWeeklyAddSchedule: boolean = false;
+  isRequestOneSchedule: boolean = false;
+  isRequestWeeklySchedule: boolean = false;
   isEditSchedule: boolean = false;
   groupedSchedules: { [key: string]: Schedule[] } = {};
   selectedSchedule!: Schedule;
   isDropdownOpenAddSchedule: boolean = false;
+  isDropdownOpenRequestSchedule: boolean = false;
   userId!: number;
   headerImage!: string;
 
@@ -261,9 +278,21 @@ export class ScheduleComponent implements OnInit{
     this.isWeeklyAddSchedule = !this.isWeeklyAddSchedule;
   }
 
+  toggleRequestOneSchedule(): void {
+    this.isDropdownOpenRequestSchedule = false;
+    this.isRequestOneSchedule = !this.isRequestOneSchedule;
+  }
+
+  toggleRequestWeeklySchedule(): void {
+    this.isDropdownOpenRequestSchedule = false;
+    this.isRequestWeeklySchedule = !this.isRequestWeeklySchedule;
+  }
+
   handleBackToSchedule(): void {
     this.isOneAddSchedule = false;
     this.isWeeklyAddSchedule = false;
+    this.isRequestOneSchedule = false;
+    this.isRequestWeeklySchedule = false;
   }
 
   toggleStartSchedule(schedule: Schedule) {
@@ -314,7 +343,10 @@ export class ScheduleComponent implements OnInit{
 
   onAddScheduleClick() {
     this.isDropdownOpenAddSchedule = !this.isDropdownOpenAddSchedule;
+  }
 
+  onRequestScheduleClick() {
+    this.isDropdownOpenRequestSchedule = !this.isDropdownOpenRequestSchedule;
   }
 
   onFilterChange() {
