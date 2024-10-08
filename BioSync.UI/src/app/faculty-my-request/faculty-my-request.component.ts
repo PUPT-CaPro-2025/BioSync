4fdea381e5ae1c73f0514ac39ef2e447c6fa6656
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-10-08 16:15:49 +0800

    add my-request component for faculty

---
+++ BioSync.UI/src/app/faculty-my-request/faculty-my-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-my-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './faculty-my-request.component.html',
+  styleUrl: './faculty-my-request.component.css'
+})
+export class FacultyMyRequestComponent {
+
+}
