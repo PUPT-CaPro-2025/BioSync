6a5fd09115bfd31057052f771db20225b5b8a18a
Jhean <drakekhendrick@gmail.com>
2024-10-08 20:48:53 +0800

    partial update on schedule request creation

--- BioSync.UI/src/app/schedule/add-schedule/add-schedule.component.ts
+++ BioSync.UI/src/app/schedule/add-schedule/add-schedule.component.ts
@@ -134,6 +134,9 @@ export class  AddScheduleComponent implements OnInit{
     this.getSections();
     this.getLaboratories();
     this.getSchoolYear();
+    if(this.isRequest){
+
+    }
   }
 
   initForm(): void{
@@ -215,6 +218,12 @@ export class  AddScheduleComponent implements OnInit{
     const target = event.target as HTMLSelectElement;
     const selectedId = Number(target.value)
     this.selectedSubject = this.subjects.find(subject => subject.id === selectedId);
+  }
+
+  setRequestingProfessor(){
+    this.scheduleForm.patchValue({
+      professor: []
+    })
   }
 
   onSchoolYearChange(event: MatSelectChange){
