aae789fed2f2146c1bb2b809086deeb5d46a159f
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-10-08 15:48:54 +0800

    add request schedule component

---
+++ BioSync.UI/src/app/schedule/request-schedule/request-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-schedule.component.html',
+  styleUrl: './request-schedule.component.css'
+})
+export class RequestScheduleComponent {
+
+}
