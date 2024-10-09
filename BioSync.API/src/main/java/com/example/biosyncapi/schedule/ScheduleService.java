9676181069295119fdc094c78f00597561abf6d4
Jhean Galope <drakekhendrick@gmail.com>
2024-10-09 12:45:09 +0800

    temp: accepting schedules on admin

--- BioSync.API/src/main/java/com/example/biosyncapi/schedule/ScheduleService.java
+++ BioSync.API/src/main/java/com/example/biosyncapi/schedule/ScheduleService.java
@@ -17,6 +17,8 @@ public interface ScheduleService {
     Optional<Schedule> getScheduleById(Long id);
     List<Schedule> createSchedule(Schedule schedule);
     List<Schedule> getAllRequestedSchedules(Long requesterId);
+    List<Schedule> getAllPendingSchedules();
     Schedule updateSchedule(Schedule schedule);
+    Schedule updatePartialSchedule(Long id, Status updates);
     void deleteSchedule(Long id);
 }
