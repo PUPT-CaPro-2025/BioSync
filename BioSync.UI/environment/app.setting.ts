8ad66b18ba6dce3c00ac59eb6140b156d98c9868
Jhean Galope <drakekhendrick@gmail.com>
2024-10-12 03:31:22 +0800

    chore: add url for flask app

--- BioSync.UI/environment/app.setting.ts
+++ BioSync.UI/environment/app.setting.ts
@@ -1,5 +1,6 @@
 export const environment = {
   apiUrl: 'http://localhost:8080',
+  serviceUrl: 'http://127.0.0.1:5000',
   key: 'helloworld',
   templateLink: 'https://docs.google.com/spreadsheets/d/1B9wCMAxM-AHHXpqnVzVzPuPKa1S-z5tDo89L6H3bTPE/edit?usp=sharing'
 };
