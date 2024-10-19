7d9019187898fac7d0c579a10f7a0ffee63176c3
Jhean Galope <drakekhendrick@gmail.com>
2024-10-19 16:16:45 +0800

    chore: rename service file name

--- BioSync.Service/recognition_service.py
+++ BioSync.Service/recognition_service.py
@@ -1,14 +1,12 @@
 import base64
 from io import BytesIO
+from typing import Optional
 
 import face_recognition
 import numpy as np
 from flask import Flask, jsonify, request
-from flask.wrappers import json
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from PIL import Image
-from sqlalchemy.sql.functions import user
 from sqlalchemy import text
 
 app = Flask(__name__)
@@ -23,22 +21,25 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String, nullable=False)
-    middle_name = db.Column(db.String, nullable=True)
-    last_name = db.Column(db.String, nullable=False)
-    usercode = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    suffix = db.Column(db.String, nullable=True)
-    email = db.Column(db.String, unique=True, nullable=False)
-    role = db.Column(db.String, nullable=False)
+    id: int = db.Column(db.Integer, primary_key=True)
+    first_name: str = db.Column(db.String, nullable=False)
+    middle_name: Optional[str] = db.Column(db.String, nullable=True)
+    last_name: str = db.Column(db.String, nullable=False)
+    usercode: str = db.Column(db.String, unique=True, nullable=False)
+    password: str = db.Column(db.String, nullable=False)
+    suffix: Optional[str] = db.Column(db.String, nullable=True)
+    email: str = db.Column(db.String, unique=True, nullable=False)
+    role: str = db.Column(db.String, nullable=False)
 
 
 class FaceEncoding(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    encoding = db.Column(db.LargeBinary, nullable=False)
+    id: int = db.Column(db.Integer, primary_key=True)
+    user_id: int = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    encoding: bytes = db.Column(db.LargeBinary, nullable=False)
 
+    def __init__(self, user_id: int, encoding: bytes) -> None:
+        self.user_id = user_id
+        self.encoding = encoding
 
 with app.app_context():
     db.create_all()
@@ -51,6 +52,10 @@ def test():
 @app.route("/encode_face", methods=["POST"])
 def encode_face():
     data = request.json
+
+    if data is None:
+        return jsonify({"status": "error", "message": "request is empty"})
+
     user = data.get("user")
     image_data = data.get("image_data")
 
@@ -70,16 +75,15 @@ def encode_face():
                 "status": "error",
                 "message": "No face found in the image."
             }), 400
-        
+
         face_encoding = face_encodings[0]
         face_encoding_bytes = face_encoding.tobytes()
         
         new_encoding = FaceEncoding(user_id=user['id'], encoding=face_encoding_bytes)
         db.session.add(new_encoding)
         db.session.commit()
-        
-        return jsonify({"status": "success", "message": "Face encoding saved successfully."}), 200
 
+        return jsonify({"status": "success", "message": "Face encoding saved successfully."}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
@@ -95,6 +99,10 @@ def get_face_encoding(user_id):
 @app.route("/recognize_face", methods=['POST'])
 def recognize_face():
     data = request.json
+
+    if data is None:
+        return jsonify({"status": "error", "message": "request is empty"})
+
     schedule_id = data.get("schedule_id")
     image_data = data.get('image_data')
     
@@ -121,34 +129,32 @@ def recognize_face():
         
         if not students:
             return jsonify({"status": "error", "message": "No students gathered"})
-        
+
         known_face_encodings = []
         known_face_user_ids = []
-        
+
         for student in students:
             student_id = student[0]
             face_encoding = get_face_encoding(student_id)
             if face_encoding is not None:
                 known_face_encodings.append(face_encoding)
                 known_face_user_ids.append(str(student_id))
-        
+
         if known_face_encodings:
             matches = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
             matched_ids = [user_id for match, user_id in zip(matches, known_face_user_ids) if match]
-        
+
             if len(matched_ids) == 0:
-                return jsonify({ "status": "error", "message": "Person Not Recognized" }), 401  
-            
+                return jsonify({ "status": "error", "message": "Person Not Recognized"}), 401
+
             return jsonify({"status": "success", "match": matched_ids[0]}), 200
         else:
             return jsonify({"status": "error", "message": "No face encodings found for students"}), 400
-    except   Exception as e:
+    except Exception as e:
         return jsonify({
             "status": "error",
             "message": str(e)
-        }) , 500
-        
-
+        }), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
