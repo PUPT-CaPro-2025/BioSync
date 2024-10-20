61d28f8a5a987903b8802e7ce46069b2ef8774f8
Jhean Galope <drakekhendrick@gmail.com>
2024-10-20 12:34:12 +0800

    chore: reformat mail codebase

--- BioSync.API/src/main/java/com/example/biosyncapi/mail/MailController.java
+++ BioSync.API/src/main/java/com/example/biosyncapi/mail/MailController.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/mail")
 public class MailController {
 
-    private final MailService mailService;
+  private final MailService mailService;
 
-    public MailController(MailService mailService) {
-        this.mailService = mailService;
+  public MailController(MailService mailService) {
+    this.mailService = mailService;
+  }
+
+  @PostMapping("/send")
+  public ResponseEntity<?> sendMessage(@RequestBody Mail mail) {
+    try {
+      mailService.sendMail(mail.getTo(), mail.getSubject(), mail.getText());
+      return ResponseEntity.ok("Email sent successfully to " + mail.getTo());
+    } catch (Exception e) {
+      return ResponseEntity.status(500).body(e.getMessage());
     }
 
-    @PostMapping("/send")
-    public ResponseEntity<?> sendMessage(@RequestBody Mail mail) {
-        try{
-            mailService.sendMail(mail.getTo(), mail.getSubject(), mail.getText());
-            return ResponseEntity.ok("Email sent successfully to " + mail.getTo());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(e.getMessage());
-        }
-
-    }
+  }
 
 }
--- BioSync.API/src/main/java/com/example/biosyncapi/mail/MailService.java
+++ BioSync.API/src/main/java/com/example/biosyncapi/mail/MailService.java
@@ -12,48 +12,51 @@ import java.util.Map;
 @Service
 public class MailService {
 
-    private final JavaMailSender mailSender;
+  private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
-    private String from;
-  
-    public MailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
+  @Value("${spring.mail.username}")
+  private String from;
+
+  public MailService(JavaMailSender mailSender) {
+    this.mailSender = mailSender;
+  }
+
+  public void sendMail(
+      String to,
+      String subject,
+      String text)
+  {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom("BioSync <" + from + ">");
+    message.setTo(to);
+    message.setSubject(subject);
+    message.setText(text);
+
+    mailSender.send(message);
+  }
+
+  public void autoSendCredentials(HashMap<User, String> credentials) {
+    for (Map.Entry<User, String> entry : credentials.entrySet()) {
+      User user = entry.getKey();
+      String password = entry.getValue();
+
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setFrom("BioSync <" + from + ">");
+      message.setTo(user.getEmail());
+      message.setSubject("BioSync Account Credentials");
+
+      String emailBody = String.format("""
+          Hello! Welcome to BioSync. Please save your account credentials below:
+                      
+          Usercode: %s \n
+          Password: %s \n
+          """, user.getUsercode(), password);
+
+      message.setText(emailBody);
+
+      mailSender.send(message);
     }
-
-    public void sendMail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("BioSync <"+ from + ">");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-
-        mailSender.send(message);
-    }
-
-    public void autoSendCredentials(HashMap<User, String> credentials) {
-        for (Map.Entry<User, String> entry : credentials.entrySet()) {
-            User user = entry.getKey();
-            String password = entry.getValue();
-
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("BioSync <" + from + ">");
-            message.setTo(user.getEmail());
-            message.setSubject("BioSync Account Credentials");
-
-            String emailBody = String.format("""
-            Hello! Welcome to BioSync. Please save your account credentials below:
-            
-            Usercode: %s \n
-            Password: %s \n
-            """, user.getUsercode(), password);
-
-            message.setText(emailBody);
-
-            mailSender.send(message);
-        }
-    }
-
+  }
 
 
 }
