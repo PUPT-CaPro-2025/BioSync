e7c8cac0a40a727ef4c2859effc2a6cbc65fc7e2
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-11-08 15:00:01 +0800

    update request-list pagination

--- BioSync.UI/src/app/request-list-schedule/request-list-schedule.component.ts
+++ BioSync.UI/src/app/request-list-schedule/request-list-schedule.component.ts
@@ -56,10 +56,10 @@ export class RequestListScheduleComponent implements OnInit {
   schedules: Schedule[] = [];
   scheduleContainer: Schedule[] = [];
 
-  @Input() totalItems: number = 500;
+  totalItems!: number;
   itemsPerPage: number = 10;
   currentPage: number = 1;
-  totalPages: number = Math.ceil(this.totalItems / this.itemsPerPage);
+  totalPages!: number;
   isOneAddSchedule: boolean = false;
   isWeeklyAddSchedule: boolean = false;
   isRequestOneSchedule: boolean = false;
@@ -109,6 +109,8 @@ export class RequestListScheduleComponent implements OnInit {
         this.filteredRepeatedSchedules();
         this.sortSchedulesById(this.schedules);
         this.setLatestSchoolYear();
+        this.totalItems = this.schedules.length;
+        this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
       },
       error: (err) => console.error(err),
     });
@@ -133,6 +135,8 @@ export class RequestListScheduleComponent implements OnInit {
         this.filteredRepeatedSchedules();
         this.sortSchedulesById(this.schedules);
         this.setLatestSchoolYear();
+        this.totalItems = this.schedules.length;
+        this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
       }
     })
   }
