38db5726a4818702d7bdd5949c4aebbc703d4381
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-11-08 13:30:01 +0800

    add purpose of visit component

---
+++ BioSync.UI/src/app/visit-purpose/visit-purpose.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-purpose',
+  standalone: true,
+  imports: [],
+  templateUrl: './visit-purpose.component.html',
+  styleUrl: './visit-purpose.component.css'
+})
+export class VisitPurposeComponent {
+
+}
