72e8ec094669c198097697a2105d642ca6dab179
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-11-08 13:59:18 +0800

    add 'add-suffix' component

---
+++ BioSync.UI/src/app/add-suffix/add-suffix.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-suffix',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-suffix.component.html',
+  styleUrl: './add-suffix.component.css'
+})
+export class AddSuffixComponent {
+
+}
