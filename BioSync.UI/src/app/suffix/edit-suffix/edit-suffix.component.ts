73b5753f3e42b65e55f655262c4e6efb2a2bc9d8
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-11-08 14:34:16 +0800

    add 'edit-suffix' component structure

--- BioSync.UI/src/app/suffix/edit-suffix/edit-suffix.component.ts
+++ BioSync.UI/src/app/suffix/edit-suffix/edit-suffix.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, Output, EventEmitter, OnInit} from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatButtonModule} from "@angular/material/button";
+import { MatSelectModule } from '@angular/material/select';
+import { Suffix } from '../../../model/suffix.model';
+import {MatDialog} from "@angular/material/dialog";
+import {PromptOkayComponent} from "../../prompt/prompt-okay/prompt-okay.component";
 
 @Component({
   selector: 'app-edit-suffix',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
   templateUrl: './edit-suffix.component.html',
-  styleUrl: './edit-suffix.component.css'
+  styleUrls: ['./edit-suffix.component.css', '../../program/add-program/add-program.component.css'],
 })
-export class EditSuffixComponent {
+export class EditSuffixComponent implements OnInit{
+  suffixForm!: FormGroup;
+  @Output() backToEditSuffix = new EventEmitter<void>();
 
+  constructor(private formBuilder: FormBuilder,
+    private dialog: MatDialog) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.suffixForm = this.formBuilder.group({
+      suffixName: ['', [Validators.required]],
+      suffixAbbreviation: ['', [Validators.required]]
+    });
+  }
+
+  returnToSuffixView(): void {
+    this.backToEditSuffix.emit();
+  }
+
+  submit(){
+    console.log('Submit button was click!');
+  }
+
+  openSuccessDialog(){
+    const ref = this.dialog.open(PromptOkayComponent, {
+      width: '400px',
+      data: {
+        title: 'Suffix Updated!',
+        message: 'Suffix details has been updated successfully.'
+      }
+    })
+
+    ref.afterClosed().subscribe({
+      next: () => {
+        this.returnToSuffixView();
+      }
+    })
+  }
 }
