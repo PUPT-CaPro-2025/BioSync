f6c7602a239f1c53caafc0227848c5666ec905e9
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-11-08 14:24:06 +0800

    add 'edit suffix' component

---
+++ BioSync.UI/src/app/suffix/edit-suffix/edit-suffix.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-suffix',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-suffix.component.html',
+  styleUrl: './edit-suffix.component.css'
+})
+export class EditSuffixComponent {
+
+}
