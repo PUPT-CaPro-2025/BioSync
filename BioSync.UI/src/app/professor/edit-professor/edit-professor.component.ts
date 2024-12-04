fa2b8eae4224f383457fcc39f808f35940903212
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-12-04 16:23:12 +0800

    feat: add camera functionality for capturing professor profile images - edit professor

--- BioSync.UI/src/app/professor/edit-professor/edit-professor.component.ts
+++ BioSync.UI/src/app/professor/edit-professor/edit-professor.component.ts
@@ -1,4 +1,4 @@
-import {Component, Output, EventEmitter, OnInit, Input, ViewEncapsulation} from '@angular/core';
+import {Component, Output, EventEmitter, OnInit, Input, ViewEncapsulation, ViewChild} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -43,6 +43,7 @@ export class EditProfessorComponent implements OnInit{
   @Output() backToEditProfessor = new EventEmitter<void>();
   @Output() editedProfessor = new EventEmitter<User>();
   @Input() professorToBeUpdated!: User;
+  @ViewChild('videoElement') videoElementRef!: any;
   //Temporary Suffixes
   allSuffix: string[] = [
     'N/A',
@@ -71,6 +72,9 @@ export class EditProfessorComponent implements OnInit{
   rightIndexState = 'Scan Right Index';
   isRightIndex = false;
   imageButtonLabel = 'Skip';
+  videoElement!: HTMLVideoElement;
+  isCameraOpen = false;
+  captureButtonLabel = 'Take Photo';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -238,6 +242,45 @@ export class EditProfessorComponent implements OnInit{
 
       this.imageButtonLabel = 'Next';
     }
+  }
+
+  openCamera() {
+    this.isCameraOpen = true;
+    this.captureButtonLabel = 'Capture Photo';
+    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+      this.videoElement = this.videoElementRef.nativeElement;
+      this.videoElement.srcObject = stream;
+      this.videoElement.play();
+    }).catch(err => {
+      // Handle error silently
+    });
+  }
+
+  capturePhoto() {
+    const canvas = document.createElement('canvas');
+    canvas.width = this.videoElement.videoWidth;
+    canvas.height = this.videoElement.videoHeight;
+    const context = canvas.getContext('2d');
+    context?.drawImage(this.videoElement, 0, 0, canvas.width, canvas.height);
+    canvas.toBlob(blob => {
+      this.selectedProfileImage = blob!;
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageSrc = reader.result;
+        this.imageButtonLabel = 'Next';
+      };
+      reader.readAsDataURL(blob!);
+    });
+    this.closeCamera();
+  }
+
+  closeCamera() {
+    this.isCameraOpen = false;
+    this.captureButtonLabel = 'Retake Photo';
+    const stream = this.videoElement.srcObject as MediaStream;
+    const tracks = stream.getTracks();
+    tracks.forEach(track => track.stop());
+    this.videoElement.srcObject = null;
   }
 
   private base64ToBlob(src: string, imagePng: string) {
