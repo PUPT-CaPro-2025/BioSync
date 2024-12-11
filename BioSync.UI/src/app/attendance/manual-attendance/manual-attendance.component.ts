3df6105b35b6b06dcd8b5088746b47704f0aa9ce
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-12-11 14:02:16 +0800

    add manual-attendance component

---
+++ BioSync.UI/src/app/attendance/manual-attendance/manual-attendance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-attendance',
+  standalone: true,
+  imports: [],
+  templateUrl: './manual-attendance.component.html',
+  styleUrl: './manual-attendance.component.css'
+})
+export class ManualAttendanceComponent {
+
+}
