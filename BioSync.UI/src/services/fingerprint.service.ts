82d4cb37f747197f3265a9f077574471cacf9515
Jhean <drakekhendrick@gmail.com>
2024-12-29 07:10:23 +0800

    feat: update fingerprint verification method to return User object

--- BioSync.UI/src/services/fingerprint.service.ts
+++ BioSync.UI/src/services/fingerprint.service.ts
@@ -4,6 +4,8 @@ import { environment } from '../../environment/app.setting';
 import { CookieService } from './cookie.service';
 import { Timein } from '../model/timein.model';
 import { Schedule } from '../model/schedule.model';
+import {Observable} from "rxjs";
+import {User} from "../model/user.model";
 
 @Injectable({
   providedIn: 'root',
@@ -65,11 +67,10 @@ export class FingerprintService {
     );
   }
 
-  verifyProfessorFingerprintForAttendance(formData: FormData) {
-    return this.http.post<string>(`${this.url}/verify/start`, formData, {
+  verifyProfessorFingerprintForAttendance(formData: FormData) : Observable<User> {
+    return this.http.post<User>(`${this.url}/verify/start`, formData, {
       headers: this.headers,
       withCredentials: true,
-      responseType: 'text' as 'json',
     });
   }
 
