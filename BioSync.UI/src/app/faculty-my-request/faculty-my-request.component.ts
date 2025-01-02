08d6060857794f613772c0cbed7210f80179fca1
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-01-02 21:20:47 +0800

    update ui and update filters

--- BioSync.UI/src/app/faculty-my-request/faculty-my-request.component.ts
+++ BioSync.UI/src/app/faculty-my-request/faculty-my-request.component.ts
@@ -16,6 +16,10 @@ import { CookieService } from '../../services/cookie.service';
 import { User } from '../../model/user.model';
 import { UserService } from '../../services/user.service';
 import jsPDF from 'jspdf';
+import { Program } from '../../model/program.model';
+import { ProgramService } from '../../services/program.service';
+import { Section } from '../../model/section.model';
+import { SectionService } from '../../services/section.service';
 
 @Component({
   selector: 'app-faculty-my-request',
@@ -30,6 +34,8 @@ import jsPDF from 'jspdf';
   providers: [
     ScheduleService,
     SchoolYearService,
+    ProgramService,
+    SectionService,
     UserService,
     CookieService,
     CryptoService,
@@ -55,6 +61,12 @@ export class FacultyMyRequestComponent implements OnInit {
   ];
   selectedSemester = 1;
 
+  programs: Program[] = [];
+  selectedProgram: number | undefined;
+
+  sections: Section[] = [];
+  selectedYearAndSection: string | undefined;
+
   schedules: Schedule[] = [];
   scheduleContainer: Schedule[] = [];
 
@@ -79,6 +91,8 @@ export class FacultyMyRequestComponent implements OnInit {
     private scheduleService: ScheduleService,
     private dialog: MatDialog,
     private schoolYearService: SchoolYearService,
+    private sectionService: SectionService,
+    private programService: ProgramService,
     private router: Router,
     private cryptoService: CryptoService,
     private cookieService: CookieService,
@@ -96,6 +110,8 @@ export class FacultyMyRequestComponent implements OnInit {
       this.getSectionId(this.userId);
     }
     this.getAcademicYears();
+    this. getAllPrograms();
+    this.getSections();
 
     this.loadImageToBase64('../../assets/header.png', (base64Image) => {
       this.headerImage = base64Image;
@@ -153,6 +169,21 @@ export class FacultyMyRequestComponent implements OnInit {
       this.selectedAcademicYear = latestSchedule.schoolYear?.id;
     }
   }
+
+  setLatestProgram() {
+    if (this.schedules && this.schedules.length > 0) {
+      const latestSchedule = this.schedules[this.schedules.length - 1];
+      this.selectedProgram = latestSchedule.section?.program.id;
+    }
+  }
+
+  setLatestYearAndSection() {
+    if (this.schedules && this.schedules.length > 0) {
+      const latestSchedule = this.schedules[this.schedules.length - 1];
+      this.selectedYearAndSection = `${latestSchedule.section?.year} - ${latestSchedule.section?.section}`;
+    }
+  }
+
 
   sortSchedulesById(schedules: Schedule[]): Schedule[] {
     return schedules.sort((a, b) => b.id - a.id);
@@ -240,6 +271,22 @@ export class FacultyMyRequestComponent implements OnInit {
         this.academicYears = academicYears;
       },
     });
+  }
+
+  getAllPrograms() {
+    this.programService.getAllPrograms().subscribe({
+      next: (programs: Program[]) => {
+        this.programs = programs;
+      }
+    })
+  }
+
+  getSections() {
+    this.sectionService.getSections().subscribe({
+      next: (sections: Section[]) => {
+        this.sections = sections;
+      }
+    })
   }
 
   get pages(): number[] {
@@ -389,9 +436,10 @@ export class FacultyMyRequestComponent implements OnInit {
 
   onFilterChange() {
     this.schedules = this.scheduleContainer.filter(
-      (schedule) =>
-        schedule.schoolYear?.id === this.selectedAcademicYear &&
-        schedule.semester?.id === this.selectedSemester,
+      schedule => schedule.schoolYear?.id === this.selectedAcademicYear
+        && schedule.semester?.id === this.selectedSemester 
+        && schedule.section?.program.id === this.selectedProgram 
+        && `${schedule.section?.year} - ${schedule.section?.section}` === this.selectedYearAndSection
     );
     this.groupSchedulesByRecurrenceId();
     this.filteredRepeatedSchedules();
