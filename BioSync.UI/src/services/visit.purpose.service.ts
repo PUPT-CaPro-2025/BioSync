92d9f6992c367f6beaaf09d372bbc177c757125f
Jhean <drakekhendrick@gmail.com>
2025-01-07 15:56:18 +0800

    feat: add service for visit purpose

---
+++ BioSync.UI/src/services/visit.purpose.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environment/app.setting";
+import {CookieService} from "./cookie.service";
+import {VisitPurpose} from "../model/visit.purpose.model";
+
+@Injectable()
+export class VisitPurposeService {
+  url: string = `${environment.apiUrl}/api/v1/visit-purposes`;
+  accessToken = this.cookieService.getCookie("authToken");
+  headers = new HttpHeaders().set('Authorization', `Bearer ${this.accessToken}`);
+
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
+
+  getVisitPurposes(){
+    return this.http.get<VisitPurpose[]>(this.url, {
+      headers: this.headers,
+      withCredentials: true
+    })
+  }
+}
