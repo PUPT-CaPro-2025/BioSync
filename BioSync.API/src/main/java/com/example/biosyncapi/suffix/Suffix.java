525d23d2625c9e412a8172b6165affffcae39ed1
Jhean <drakekhendrick@gmail.com>
2025-01-07 11:39:37 +0800

    feat: create suffix object model

---
+++ BioSync.API/src/main/java/com/example/biosyncapi/suffix/Suffix.java
@@ -0,0 +1,45 @@
+package com.example.biosyncapi.suffix;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Suffix {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String abbreviation;
+
+    public Suffix(){}
+
+    public Suffix(String name, String abbreviation) {
+        this.name = name;
+        this.abbreviation = abbreviation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+}
