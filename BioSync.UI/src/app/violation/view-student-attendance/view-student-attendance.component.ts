6990b4381313d8d59fcd491701f2c17407065138
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-01-10 19:21:22 +0800

    add view student attendance component

---
+++ BioSync.UI/src/app/violation/view-student-attendance/view-student-attendance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-student-attendance',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-student-attendance.component.html',
+  styleUrl: './view-student-attendance.component.css'
+})
+export class ViewStudentAttendanceComponent {
+
+}
