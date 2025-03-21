5fbb2f3bfef62ecabdecd31ceadb2e7977e5a8c5
Jhean Galope <drakekhendrick@gmail.com>
2025-03-21 18:32:44 +0800

    feat: add unit test for suffix

--- BioSync.API/src/main/java/com/example/biosyncapi/suffix/Suffix.java
+++ BioSync.API/src/main/java/com/example/biosyncapi/suffix/Suffix.java
@@ -23,6 +23,10 @@ public class Suffix {
         this.abbreviation = abbreviation;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
     }
