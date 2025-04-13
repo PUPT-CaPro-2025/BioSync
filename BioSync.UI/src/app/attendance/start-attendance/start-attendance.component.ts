9ef4f02eb9fbc9d69e5e4832d5c4c7efaaa5a523
Jhean Galope <drakekhendrick@gmail.com>
2025-04-13 22:34:15 +0800

    feat: manual input attendance

--- BioSync.UI/src/app/attendance/start-attendance/start-attendance.component.ts
+++ BioSync.UI/src/app/attendance/start-attendance/start-attendance.component.ts
@@ -26,6 +26,9 @@ import { CookieService } from '../../../services/cookie.service';
 import { UserService } from '../../../services/user.service';
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {CryptoService} from "../../../services/crypto.service";
+import {
+  LogAttendanceComponent
+} from "../../prompt/log-attendance/log-attendance.component";
 
 @Component({
   selector: 'app-start-attendance',
@@ -189,7 +192,7 @@ export class StartAttendanceComponent implements OnInit {
           setTimeout(() => {
             this.reminder = 'Scan Student Fingerprint';
             this.hasProfessorVerified = true;
-            this.loggedProfessor = null;
+            //this.loggedProfessor = null;
             this.profileImageUrl = '';
             this.isSuccess = false;
           }, 3000);
@@ -207,7 +210,7 @@ export class StartAttendanceComponent implements OnInit {
 
   private setLoadingProf(){
     this.loading = true;
-    this.loggedProfessor = null;
+    //this.loggedProfessor = null;
     this.profileImageUrl = '';
     this.isSuccess = false;
     this.isError = false;
@@ -391,6 +394,26 @@ export class StartAttendanceComponent implements OnInit {
       next: (result) => {
         if (!result) return;
         this.stopAttendance(schedule);
+      },
+    });
+  }
+
+  openLogAttendanceDialog(schedule: Schedule) {
+    const ref = this.dialog.open(LogAttendanceComponent, {
+      width: '400px',
+      data: {
+        title: 'Log Student',
+        scheduleId: schedule.id,
+      },
+    });
+
+    ref.afterClosed().subscribe({
+      next: (result) => {
+        if(this.isTimeOut) {
+          this.sendBarcodeTimeOut(result);
+        } else {
+          this.sendBarcodeTimeIn(result);
+        }
       },
     });
   }
@@ -609,7 +632,7 @@ export class StartAttendanceComponent implements OnInit {
       setTimeout(() => {
         this.reminder = 'Scan Student Fingerprint';
         this.hasProfessorVerified = true;
-        this.loggedProfessor = null;
+        //this.loggedProfessor = null;
         this.profileImageUrl = '';
         this.isSuccess = false;
       }, 3000);
